e536a2737211b9ede127d6e56b34aea8ced94be9
jzhang1198 <jzhang1198@gmail.com>
2022-02-09 08:54:16 -0800

    Updated __init__.py

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,7 +1,15 @@
+"""
+BMI203: Biocomputing algorithms Winter 2022
+Assignment 5: K means clustering
+"""
+
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
         make_clusters, 
         plot_clusters,
         plot_multipanel)
+
+__version__= '0.1.0'
+
 
